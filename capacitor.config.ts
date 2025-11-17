45861772b384ff40e85d9c83212053a0e7400a19
DaveDominicLactuan <davedominic.lactuan.22@usjr.edu.ph>
2025-11-17 14:58:18 +0800

    with splash screen and icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'thesisProject',
+  appName: 'D.A.M.A.G.E Camera',
   webDir: 'www',
 
   server: {
